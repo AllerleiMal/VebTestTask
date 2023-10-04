df5503c6e834585bf48a6f6f74db6e3faed6c10e
AllerleiMal <emelpavel2002@gmail.com>
2023-10-05 01:26:06 +0300

    feat: create User and Role models

---
+++ VebTestTask/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace VebTestTask.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public int Age { get; set; }
+    public string? Email { get; set; }
+    private IEnumerable<Role>? Roles { get; set; }
+}
