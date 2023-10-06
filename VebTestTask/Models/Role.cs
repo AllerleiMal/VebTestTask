cc9fc5d48e301fd29474510bb1b498be671584d3
AllerleiMal <emelpavel2002@gmail.com>
2023-10-06 15:26:12 +0300

    feat: create DbContext and models relations

--- VebTestTask/Models/Role.cs
+++ VebTestTask/Models/Role.cs
@@ -1,7 +1,11 @@
-﻿namespace VebTestTask.Models;
+﻿using Newtonsoft.Json;
+
+namespace VebTestTask.Models;
 
 public class Role
 {
     public int Id { get; set; }
     public string? Name { get; set; }
+    [JsonIgnore]
+    public ICollection<User> Users { get; set; }
 }
