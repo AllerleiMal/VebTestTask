f6474c6b640ca07c513477c58592ee82b414d4e7
AllerleiMal <emelpavel2002@gmail.com>
2023-10-08 03:58:36 +0300

    feat: create response wrappers

---
+++ VebTestTask/Wrapper/PagedResponse.cs
@@ -0,0 +1,21 @@
+﻿namespace VebTestTask.Wrapper;
+
+public class PagedResponse<T> : Response<T>
+{
+    public int PageNumber { get; set; }
+    public int PageSize { get; set; }
+    public long TotalRecords { get; set; }
+    
+    public PagedResponse(T data, int pageNumber, int pageSize, long totalRecords) : base(data)
+    {
+        PageNumber = pageNumber;
+        PageSize = pageSize;
+        TotalRecords = totalRecords;
+    }
+
+    public PagedResponse(): base(default)
+    {
+        PageNumber = 1;
+        PageSize = 10;
+    }
+}
