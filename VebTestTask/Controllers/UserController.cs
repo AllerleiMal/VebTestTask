c9b2783dca94ab68e5c35141533f377d882f39be
AllerleiMal <emelpavel2002@gmail.com>
2023-10-11 13:36:10 +0300

    docs: add API documentation with Swagger

--- VebTestTask/Controllers/UserController.cs
+++ VebTestTask/Controllers/UserController.cs
@@ -28,11 +28,16 @@ public class UserController : ControllerBase
     }
 
     /// <summary>
-    /// Get all Users
+    /// Gets Users after applying filtering, ordering and pagination 
     /// </summary>
-    /// <returns></returns>
+    /// <returns>Response with List of Users of one page</returns>
+    /// <response code="200">All query parameters are correct, returns requested Users</response>
+    /// <response code="400">Incorrect query parameters</response>
+    /// <response code="500">Unhandled exception during request processing</response>
     [HttpGet]
     [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     public async Task<IActionResult> GetUsersAsync([FromQuery] PaginationFilter filter)
     {
         var pagedUsersParams = await PaginatedUsersParams.GetParamsFromPaginationFilter(filter);
@@ -55,14 +60,17 @@ public class UserController : ControllerBase
     }
 
     /// <summary>
-    /// Get concrete user by ID
+    /// Gets concrete user by ID
     /// </summary>
     /// <param name="id">Unique user ID</param>
     /// <returns>User with entered ID and attached roles</returns>
     /// <response code="200">User with such ID found successfully</response>
+    /// <response code="404">User with such ID found successfully</response>
+    /// <response code="500">Unhandled exception during request processing</response>
     [HttpGet("{id:int}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     public async Task<IActionResult> GetUserByIdAsync(int id)
     {
         var targetUser = await _userRepository.GetUserByIdAsync(id);
@@ -80,9 +88,13 @@ public class UserController : ControllerBase
     /// </summary>
     /// <param name="id">Unique user id</param>
     /// <returns></returns>
+    /// <response code="204">Specified user deleted successfully</response>
+    /// <response code="404">Specified user is not found</response>
+    /// <response code="500">Unhandled exception during request processing</response>
     [HttpDelete("{id:int}")]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     public async Task<IActionResult> DeleteUserByIdAsync(int id)
     {
         var targetUser = await _userRepository.DeleteUserAsync(id);
@@ -94,8 +106,17 @@ public class UserController : ControllerBase
         return NoContent();
     }
 
+    /// <summary>
+    /// Creates User with form
+    /// </summary>
+    /// <param name="user">User entity from request body</param>
+    /// <returns>Created User</returns>
+    /// <response code="201">User created successfully</response>
+    /// <response code="400">Provided User is null or there is a validation error</response>
+    /// <response code="500">Unhandled exception during request processing</response>
     [HttpPost]
     [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     public async Task<IActionResult> CreateUserAsync([FromBody] User? user)
     {
@@ -124,16 +145,42 @@ public class UserController : ControllerBase
         return CreatedAtAction(nameof(GetUserByIdAsync), new { id = addedUser.Id }, new Response<User?>(addedUser));
     }
 
+    /// <summary>
+    /// Updates specified User
+    /// </summary>
+    /// <param name="userToUpdate">Updated user from request body</param>
+    /// <returns></returns>
+    /// <response code="204">Updates applied successfully</response>
+    /// <response code="404">No user with such ID</response>
+    /// <response code="400">Provided User is null or there is a validation error</response>
+    /// <response code="500">Unhandled exception during request processing</response>
     [HttpPut]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
-    [ProducesResponseType(StatusCodes.Status201Created)]
-    public async Task<ActionResult> UpdateUserAsync([FromBody] User userToUpdate)
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<ActionResult> UpdateUserAsync([FromBody] User? userToUpdate)
     {
+        if (userToUpdate is null)
+        {
+            return BadRequest(
+                new Response<User>
+                {
+                    Message = "No user entity in request's body",
+                    Succeeded = false
+                });
+        }
+
         var targetUser = await _userRepository.GetUserByIdAsync(userToUpdate.Id);
 
         if (targetUser is null)
         {
-            return NotFound($"User with id {userToUpdate.Id} not found.");
+            return NotFound(
+                new Response<User>
+                {
+                    Message = $"User with id {userToUpdate.Id} not found.",
+                    Succeeded = false
+                });
         }
 
         var result = await _userValidator.ValidateAsync(userToUpdate);
@@ -152,10 +199,18 @@ public class UserController : ControllerBase
 
         await _userRepository.UpdateUserAsync(userToUpdate);
 
-        return CreatedAtAction(nameof(GetUserByIdAsync), new { id = userToUpdate.Id }, new Response<User?>());
+        return NoContent();
     }
 
+    /// <summary>
+    /// Gets all defined roles
+    /// </summary>
+    /// <returns>List of defined Roles</returns>
+    /// <response code="200">Returns all defined Roles</response>
+    /// <response code="500">Unhandled exception during request processing</response>
     [HttpGet("roles")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     public async Task<IActionResult> GetAllRolesAsync()
     {
         var result = await _roleRepository.GetRolesAsync();
@@ -165,23 +220,44 @@ public class UserController : ControllerBase
         });
     }
 
+    /// <summary>
+    /// Adds Role with specified ID to the User with specified ID
+    /// </summary>
+    /// <param name="userId">Target User ID</param>
+    /// <param name="newRoleId">Target Role ID</param>
+    /// <returns>Changed User</returns>
+    /// <response code="200">Role added to the target User</response>
+    /// <response code="304">Target User already has specified Role, no data modification</response>
+    /// <response code="404">Target User or Role is not found</response>
+    /// <response code="500">Unhandled exception during request processing</response>
     [HttpGet("add_role")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status304NotModified)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
-    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     public async Task<IActionResult> AddNewRole(int userId, int newRoleId)
     {
         var targetUser = await _userRepository.GetUserByIdAsync(userId);
         if (targetUser is null)
         {
-            return NotFound($"No user with such id {userId}");
+            return NotFound(
+                new Response<User>
+                {
+                    Message = $"No user with such id {userId}.",
+                    Succeeded = false
+                });
         }
 
         var targetRole = await _roleRepository.GetRoleByIdAsync(newRoleId);
 
         if (targetRole is null)
         {
-            return NotFound($"No role with such id {newRoleId}");
+            return NotFound(
+                new Response<User>
+                {
+                    Message = $"No role with such id {newRoleId}.",
+                    Succeeded = false
+                });
         }
 
         var changedUser = await _userRepository.AddNewRoleForUser(targetUser, targetRole);
